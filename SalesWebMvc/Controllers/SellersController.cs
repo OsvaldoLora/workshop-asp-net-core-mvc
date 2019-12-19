45ccf76f72e73fc4c8d74909c70c400502a9c57f
Osvaldo Lora <jolnmtb@gmail.com>
2019-12-19 19:23:56 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -22,5 +23,19 @@ namespace SalesWebMvc.Controllers
 
             return View(list);
         }
-    }
+
+		public IActionResult Create()
+		{			
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));//poderia ser ("Index"), porém essa forma é melhor para fazer manutenções futuras no site, caso o nome da index precise ser alterado não precisaremos alterar essa linha de código
+		}
+
+	}
 }
