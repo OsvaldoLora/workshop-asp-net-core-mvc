ef03c0ca496eecffb6565c1bf707f38108fbb57a
Osvaldo Lora <jolnmtb@gmail.com>
2019-12-28 08:21:01 -0300

    Seller details and eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -28,7 +29,7 @@ namespace SalesWebMvc.Services
 		{
 			//var obj = _context.Seller.Find(id);
 			//return obj;
-			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+			return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id); //o include foi adicionado para trazer o nome do departamento na view Details dos Sellers (é um join das tabelas)
 			
 		}
 
